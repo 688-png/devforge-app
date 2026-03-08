2ea862826343499d0d4631f081136ffc696910f9
688-png <brianmakau688@gmail.com>
2026-03-09 01:20:48 +0300

    Update src/App.tsx from DevForge

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// Welcome to DevForge
+// Start by describing your app in the chat panel
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <h1>Hello DevForge</h1>
+    </div>
+  );
+}
